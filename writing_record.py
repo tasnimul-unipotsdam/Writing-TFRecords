1d8a7241cb93585ed4c63c33ebfd6dd55be9b53e
Tasnimul Hasan <tasnimul.abir@gmail.com>
2020-12-15 18:11:30 +0100

    Update writing_record.py
    adding CLAHE to images

--- writing_record.py
+++ writing_record.py
@@ -1,16 +1,10 @@
 import os
 import glob
-import json
 import cv2
-from pprint import pprint
-
-
 import numpy as np
 import tensorflow as tf
-from sklearn.utils import class_weight
 from tqdm import tqdm
 
-from collections import Counter
 
 
 def _int64_feature(value):
@@ -22,30 +16,18 @@ def _bytes_feature(value):
 
 
 def encode_label(label):
-    if label == "AnnualCrop":
+    if label == "Cassava Bacterial Blight":
         return 0
-    if label == "Forest":
+    if label == "Cassava Brown Streak Disease":
         return 1
-    if label == 'HerbaceousVegetation':
+    if label == 'Cassava Green Mottle':
         return 2
-    if label == "Highway":
+    if label == "Cassava Mosaic Disease":
         return 3
-    if label == "Industrial":
+    if label == "Healthy":
         return 4
-    if label == "Pasture":
-        return 5
-    if label == "PermanentCrop":
-        return 6
-    if label == "Residential":
-        return 7
-    if label == "River":
-        return 8
-    if label == "SeaLake":
-        return 9
-
 
 def _process_examples(example_data, filename: str, channels=3):
-
     with tf.io.TFRecordWriter(filename) as writer:
         for i, ex in enumerate(example_data):
             image = ex['image'].tostring()
@@ -60,6 +42,12 @@ def _process_examples(example_data, filename: str, channels=3):
     return None
 
 
+def shard_dataset(dataset, num_records=20):
+    chunk = len(dataset) // num_records
+    parts = [(k * chunk) for k in range(len(dataset)) if (k * chunk) < len(dataset)]
+    return chunk, parts
+
+
 class WritingTFRecord(object):
     def __init__(self, data_path, **kwargs):
         self.data_path = data_path
@@ -69,52 +57,48 @@ class WritingTFRecord(object):
         img_dir = os.path.join(self.data_path, data_type)
         labels = os.listdir(img_dir)
         data = []
-        for l in tqdm(labels):
-            list_image_fns = glob.glob(os.path.join(img_dir, l, '*'))
+        for label in tqdm(labels):
+            list_image_fns = glob.glob(os.path.join(img_dir, label, '*'))
             for fn in list_image_fns:
-                img = cv2.imread(fn, cv2.IMREAD_COLOR)
-                img = cv2.resize(img, (64, 64))
+                image = cv2.imread(fn, 1)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
+                lab_planes = cv2.split(lab)
+                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+                lab_planes[0] = clahe.apply(lab_planes[0])
+                merge = cv2.merge(lab_planes)
+                img = cv2.cvtColor(merge, cv2.COLOR_LAB2RGB)
+                img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+                img = img.astype(np.float32)
                 meta = {
                     'image': img,
-                    'label': l,
+                    'label': label,
                     'data_type': data_type
                 }
                 data.append(meta)
-        """Class Weights for Imbalanced Dataset"""
-        y = np.array([ex['label'] for ex in data])
-        class_weights = class_weight.compute_class_weight("balanced", np.unique(y), y)
-        class_weights = dict(enumerate(class_weights))
-        print(class_weights)
-        dict_file = open("weights.json", "w")
-        json.dump(class_weights, dict_file)
-        dict_file.close()
-        pprint('number of samples in {}: {}'.format(data_type, len(data)))
-        pprint('data statistics {}'.format(dict(Counter([ex['label'] for ex in data]))))
+
+        print('Finished reading for {} from the array....'.format(data_type))
         return data
 
-    def shard_dataset(self, dataset, num_records=20):
-        chunk = len(dataset) // num_records
-        parts = [(k * chunk) for k in range(len(dataset)) if (k * chunk) < len(dataset)]
-        return chunk, parts
-
-    def save_data(self, dataset, label, dataname='euro_sat', data_type='train'):
+    def save_data(self, dataset, label, data_name='Cassava', data_type='train'):
         train_check = 0
         if len(dataset) > 100:
-            chunk, parts = self.shard_dataset(dataset)
+            chunk, parts = shard_dataset(dataset)
             for i, j in enumerate(tqdm(parts)):
                 shard = dataset[j:(j + chunk)]
-                fn = '{}_{}-{}_{:03d}-{:03d}.tfrecord'.format(data_type, label, dataname, i + 1, len(parts))
-                _process_examples(shard, os.path.join(self.data_path, 'records', fn))
+                fn = '{}_{}-{}_{:03d}-{:03d}.tf_record'.format(data_type, label, data_name, i + 1, len(parts))
+                _process_examples(shard, os.path.join(self.data_path, 'tf_records_224', fn))
                 train_check += len(shard)
             print('Number of saved samples for {}: {}'.format(label, train_check))
         else:
-            fn = '{}_{}-{}_{:03d}-{:03d}.tfrecord'.format(data_type, label, dataname, 1, 1)
-            _process_examples(dataset, os.path.join(self.data_path, 'records', fn))
+            fn = '{}_{}-{}_{:03d}-{:03d}.tf_record'.format(data_type, label, data_name, 1, 1)
+            _process_examples(dataset, os.path.join(self.data_path, 'tf_records_224', fn))
             print('Small dataset with {} samples'.format(len(dataset)))
         return None
 
     def preprocess_data(self):
         for d in ['validation', 'train']:
+            print("Working for {}...".format(d))
             data = self.load_dataset(data_type=d)
             labels = list(set([ex['label'] for ex in data]))
             for l in labels:
@@ -123,5 +107,5 @@ class WritingTFRecord(object):
 
 
 if __name__ == '__main__':
-    prep = WritingTFRecord("D:\\PROJECTS\\Writing-TFRecords")
+    prep = WritingTFRecord("D:\\PROJECTS\\Casava_Classification\\DataSet")
     prep.preprocess_data()
