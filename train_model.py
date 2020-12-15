b5a35a07cdc6f6b73d1de447d644dc692e69717b
Tasnimul Hasan <tasnimul.abir@gmail.com>
2020-12-15 18:14:37 +0100

    Update train_model.py
    add sequential augmentation to images

--- train_model.py
+++ train_model.py
@@ -2,108 +2,102 @@ import os
 import random
 
 import datetime
+import numpy as np
 import matplotlib.pyplot as plt
-
 import tensorflow as tf
-from tensorflow.keras.losses import categorical_crossentropy
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.regularizers import l2
-
+from focal_loss import SparseCategoricalFocalLoss
 from reading_record import DataLoader
-
 random.seed(1001)
 
+TF_ENABLE_GPU_GARBAGE_COLLECTION = False
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 devises = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(devises[0], True)
 
-with tf.device("/gpu:0"):
-    inputs = tf.keras.Input(shape=(64, 64, 3))
-    x = inputs
-    x = tf.keras.layers.Conv2D(64, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+train_dataset = DataLoader("D:\\PROJECTS\Casava_Classification\\DataSet\\tf_records_224", training=True).balanced_train_dataset()
+validation_dataset = DataLoader("D:\\PROJECTS\Casava_Classification\\DataSet\\tf_records_224", training=False).validation_dataset()
 
-    x = tf.keras.layers.Conv2D(64, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPool2D()(x)
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal_and_vertical'),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+  tf.keras.layers.experimental.preprocessing.RandomZoom(0.1)
+])
 
-    x = tf.keras.layers.Conv2D(128, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+def compile_model():
+    base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
 
-    x = tf.keras.layers.Conv2D(128, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPool2D()(x)
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = data_augmentation(inputs)
+    x = tf.keras.applications.mobilenet.preprocess_input(x)
+    x = base_model(x, training=False)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    x = tf.keras.layers.Dense(5, activation='softmax')(x)
+    model = tf.keras.Model(inputs, x)
 
-    x = tf.keras.layers.Conv2D(256, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
+    # base_model.trainable = False
 
-    x = tf.keras.layers.Conv2D(256, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPool2D()(x)
+    for layer in base_model.layers[:100]:
+      layer.trainable =  False
+    for layer in base_model.layers[100:]:
+      layer.trainable =  True
 
-    x = tf.keras.layers.Conv2D(512, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
 
-    x = tf.keras.layers.Conv2D(512, 3, padding="same", kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.MaxPool2D()(x)
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+                  # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                  loss= SparseCategoricalFocalLoss(gamma=2),
+                  # loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False, label_smoothing=0.05),
+                  metrics=['accuracy'])
+    model.summary()
+    return model
 
-    x = tf.keras.layers.Flatten()(x)
+def train_model(model):
+    log_dir = "D:\\PROJECTS\\Casava_Classification\\logs\\fit\\MobileNetV2_6" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    board = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    x = tf.keras.layers.Dense(64, kernel_regularizer=l2(.0001))(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Dropout(.5)(x)
 
-    x = tf.keras.layers.Dense(10, activation="softmax")(x)
+    class_weights = {0: 4.078495660559306, 1: 1.9772791023842917,
+                    2: 1.8105308219178082, 3: 0.3226579188281965,
+                    4: 1.6736842105263159}
 
-    model = tf.keras.Model(inputs, x, name='model')
+    steps_epoch = np.ceil(21147  / 64)
+    valid_steps = np.ceil(100  / 32)
 
-model.summary()
-log_dir = "logs\\fit\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-board = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    history = model.fit(train_dataset,
+                        epochs=100,
+                        verbose=2,
+                        callbacks=[board],
+                        validation_data=validation_dataset,
+                        class_weight=class_weights,
+                        steps_per_epoch=steps_epoch,
+                        workers= 12
+                        )
 
-model.compile(optimizer=Adam(learning_rate=0.0001), loss=categorical_crossentropy, metrics=["accuracy"])
+    model.save("D:\\PROJECTS\\Casava_Classification\\model_plot\\MobileNetV2_6.h5")
 
-train_dataset = DataLoader("D:\\PROJECTS\\EuroSAT\\records", training=True).general_dataset()
-validation_dataset = DataLoader("D:\\PROJECTS\\EuroSAT\\records", training=False).general_dataset()
+    fig = plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
 
-dict_file = open("weights.json", "r")
-class_weight = dict_file.read()
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
+    plt.show()
+    fig.savefig("D:\\PROJECTS\\Casava_Classification\\model_plot\\MobileNetV2_6.jpg")
 
-history = model.fit(train_dataset, verbose=2, epochs=150, class_weight=class_weight,
-                    validation_data=validation_dataset, callbacks=[board])
 
-train_score = model.evaluate(train_dataset, verbose=0)
-print('train loss, train acc:', train_score)
 
-validation_score = model.evaluate(validation_dataset, verbose=0)
-print('validation loss, validation acc:', validation_score)
-
-model.save("model.h5")
-
-fig = plt.figure()
-plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'])
-
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'validation'])
-plt.show()
-fig.savefig("accuracy_and_loss")
+if __name__ == '__main__':
+    compile_model = compile_model()
+    train_model(compile_model)
+    pass
